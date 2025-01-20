2bcd6725275ad582dda9a21c5a1a1ee6e8b7afa7
Sumit Khandelwal <synergy2411@outlook.com>
2025-01-20 16:00:46 +0530

    scalar types

--- 02-graphql-basics/src/server.js
+++ 02-graphql-basics/src/server.js
@@ -1,17 +1,29 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { createServer } from "node:http";
 
+// Scalar Types - String, Int, Boolean, Float and ID
+
 // typeDefs - define capabilities of server
 const typeDefs = `
     type Query {
-        hello: String
+        hello: String!
+        age: Int
+        isAdmin: Boolean
+        salary: Float
+        userId: ID
+        friends: [String!]!
     }
 `;
 
 // resolvers - implementation of typeDefs
 const resolvers = {
   Query: {
-    hello: () => "GraphQL Server!",
+    hello: () => "World!",
+    age: () => 23,
+    isAdmin: () => true,
+    salary: () => 123.34,
+    userId: () => "jsdh-76576-sdfsdf-sd878",
+    friends: () => ["Joey", null, "Ross", "Rachel"],
   },
 };
 
