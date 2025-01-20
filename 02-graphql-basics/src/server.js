1e52a11b2a149a2e3ccb00bd836a755a5c891123
Sumit Khandelwal <synergy2411@outlook.com>
2025-01-20 17:11:51 +0530

    arguments added

--- 02-graphql-basics/src/server.js
+++ 02-graphql-basics/src/server.js
@@ -6,18 +6,44 @@ const users = [
   { id: "u002", name: "rachel green", age: 22 },
   { id: "u003", name: "chandler bing", age: 24 },
 ];
+
+// Post should contain id, title, body and published fields
+const posts = [
+  { id: "p001", title: "GraphQL 101", body: "Awesome Blog", published: true },
+  {
+    id: "p002",
+    title: "Spring in Java",
+    body: "Like it❤️❤️",
+    published: false,
+  },
+  { id: "p003", title: "Refresh React", body: "Very fast", published: true },
+  {
+    id: "p004",
+    title: "Beginning with Node",
+    body: "NodeJS Fundamentals",
+    published: false,
+  },
+];
+
 // Scalar Types - String, Int, Boolean, Float and ID
 
 // typeDefs - define capabilities of server
 const typeDefs = /* GraphQL */ `
   type Query {
     hello: String!
-    users: [User!]!
+    users(query: String): [User!]!
+    posts: [Post!]!
   }
   type User {
     id: ID!
     name: String!
     age: Int!
+  }
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
@@ -25,7 +51,15 @@ const typeDefs = /* GraphQL */ `
 const resolvers = {
   Query: {
     hello: () => "World!",
-    users: () => users,
+    users: (parent, args, context, info) => {
+      if (args.query) {
+        return users.filter((user) =>
+          user.name.includes(args.query.toLowerCase())
+        );
+      }
+      return users;
+    },
+    posts: () => posts,
   },
 };
 
